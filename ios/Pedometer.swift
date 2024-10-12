0499b17b399b3335547e91c62cb679ea69920c2f
Jaswant Kushwaha <jaswantkushwaha@192.168.1.21>
2024-10-12 16:43:55 +0530

    test(ios): set up example tests

--- ios/Pedometer.swift
+++ ios/Pedometer.swift
@@ -4,39 +4,59 @@ import React
 
 @objc(ReactNativePedometer)
 class ReactNativePedometer: NSObject {
-  
-  private let pedometer = CMPedometer()
-  
-  @objc(startPedometerUpdates:rejecter:)
-  func startPedometerUpdates(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-    guard CMPedometer.isStepCountingAvailable() else {
-      reject("Pedometer Not Available", "This device does not support pedometer data", nil)
-      return
+    
+    private let pedometer = CMPedometer()
+    
+    // Create a bridge reference
+    private var bridge: RCTBridge?
+
+    // This method is called when the module is initialized
+    @objc
+    func setBridge(_ bridge: RCTBridge) {
+        self.bridge = bridge
+    }
+
+    @objc(startPedometerUpdates:rejecter:)
+    func startPedometerUpdates(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        guard CMPedometer.isStepCountingAvailable() else {
+            reject("Pedometer Not Available", "This device does not support pedometer data", nil)
+            return
+        }
+        
+        pedometer.startUpdates(from: Date()) { (pedometerData, error) in
+            if let error = error {
+                reject("Pedometer Error", error.localizedDescription, error)
+                return
+            }
+            
+            if let data = pedometerData {
+                let pedometerData: [String: Any] = [
+                    "steps": data.numberOfSteps.intValue,
+                    "distance": data.distance?.doubleValue ?? 0.0
+                ]
+                // Sending events to JavaScript
+                self.sendPedometerData(pedometerData: pedometerData)
+            }
+        }
     }
     
-    pedometer.startUpdates(from: Date()) { (pedometerData, error) in
-      if let error = error {
-        reject("Pedometer Error", error.localizedDescription, error)
-        return
-      }
-      
-      if let data = pedometerData {
-        let pedometerData: [String: Any] = [
-          "steps": data.numberOfSteps.intValue,
-          "distance": data.distance?.doubleValue ?? 0.0
-        ]
-        resolve(pedometerData)
-      }
+    @objc(stopPedometerUpdates)
+    func stopPedometerUpdates() {
+        pedometer.stopUpdates()
     }
-  }
-  
-  @objc(stopPedometerUpdates)
-  func stopPedometerUpdates() {
-    pedometer.stopUpdates()
-  }
-  
-  @objc
-  static func requiresMainQueueSetup() -> Bool {
-    return true
-  }
+    
+    private func sendPedometerData(pedometerData: [String: Any]) {
+        // Check if bridge is available
+        guard let bridge = bridge else {
+            return
+        }
+        
+        // Send event to JavaScript
+        bridge.eventDispatcher().sendAppEvent(withName: "PedometerData", body: pedometerData)
+    }
+
+    @objc
+    static func requiresMainQueueSetup() -> Bool {
+        return true
+    }
 }
